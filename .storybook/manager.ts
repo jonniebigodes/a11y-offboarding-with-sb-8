13f0d378b8c2bb9eeba6afe7c0f30b46bbe618b4
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-06-25 17:56:41 +0100

    Fix manager and git ignore

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -3,13 +3,7 @@ import { create } from '@storybook/theming/create'
 // @ts-ignore-next-line
 import chromaticLogo from '../src/assets/images/logo-chromatic.svg'
 
-const theme = create({
-  base: 'dark',
-  brandImage: chromaticLogo,
-})
 
 addons.setConfig({
-  theme,
-  showPanel: true,
-  panelPosition: 'right',
+  panelPosition: 'bottom',
 })
